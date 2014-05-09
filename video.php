b56d14509eed592dc0936a1b9c3e4841ec0e16ee
phelixbtc <github@blockchained.com>
2014-05-09 09:46:27 +0200

    Video

---
+++ video.php
@@ -0,0 +1,3 @@
+<div class="row">
+    <iframe width="697" height="523" src="https://www.youtube-nocookie.com/embed/6OFv4fHsZQ0?vq=hd720&autoplay=1&rel=0" frameborder="0" allowfullscreen></iframe>
+</div>
