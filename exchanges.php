80c28acbb73cd30a78635a6f023bfec0d862e84a
phelixbtc <github@blockchained.com>
2014-05-09 11:30:38 +0200

    Levels to exchanges page

--- exchanges.php
+++ exchanges.php
@@ -1,10 +1,29 @@
 <div class="row">
     <p><span style="font-size:130%;">&#8644;</span> You can buy and sell <b>Namecoins</b> here:</p>
-    <p>...</a></p>
-    <p>...</p>
-    <p>...</p>
-    <p>...</p>
-    <p>...</p>
+    <span style="font-size:130%;">
+        <p><b>Gold</b></p>
+        <p>...</p>
+    </span>
+    <span style="font-size:120%;">
+        <p><b>Silver</b></p>
+        <p>...</p>
+    </span>
+    <span style="font-size:110%;">
+        <p><b>Bronze</b></p>
+        <p>...</p>
+    </span>
+    <span style="font-size:100%;">
+        <b>Basic</b><br>
+        <a target="_blank" href="https://btc-e.com">btc-e</a><br>
+        <a target="_blank" href="https://coinmkt.com">coinMKT</a><br>
+        <a target="_blank" href="https://cryptonit.net">Cryptonit</a><br>
+        <a target="_blank" href="https://kraken.com">Kraken</a><br>
+        <a target="_blank" href="http://liliontransfer.org">Lilion Transfer</a><br>
+        <a target="_blank" href="https://www.vaultofsatoshi.com">Vault of Satoshi</a><br>
+        <a target="_blank" href="https://vircurex.com">Vircurex</a><br>
+        <a target="_blank" href="https://24change.com">24change</a><br>
+        <br>
+    </span>
     <br>
-    <p>These are paid ads. The higher the payment the higher the place in the list. To get on the list contact exchanges a|t namecoin.info</p>
+    <p>Gold, silver and bronze level exchanges donate to the Namcoin project. The higher the donation the higher the level / place in the list and the larger the logo. To get on the list contact exchanges a|t namecoin.info</p>
 </div>
