10cb58454bb19334d85b686ceb48e34799cbbf18
cassiniNMC <cassiniNMC@users.noreply.github.com>
2014-08-12 23:31:49 +0200

    namecoind 0.3.75 for Mac OS 10.9.x (Mavericks)

--- download.php
+++ download.php
@@ -8,7 +8,8 @@ Download <a href="http://namecoin.info/files/Namecoin_v0.3.75_setup.exe">v0.3.75
 <a target="_blank" href="http://forum.namecoin.info/viewtopic.php?f=8&t=1865">Release information</a>.</div>
 <div class="col-sm-4">
 <h3>Mac OS X</h3>
-Coming Soon!
+Download <a href="http://namecoin.info/files/namecoind-mavericks-0.3.75-build20140808.zip">v0.3.75 namecoind</a>.<br>
+<a target="_blank" href="http://forum.namecoin.info/viewtopic.php?f=8&t=TO_BE_ANNOUNCED">Release information</a>.</div>
 </div>
 </ul>
 </div>
